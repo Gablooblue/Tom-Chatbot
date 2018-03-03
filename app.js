499b04296e3bb231c545473b0b27fe89bc9c3ba7
cheskape <peraltacheska@gmail.com>
2018-03-04 03:17:28 +0800

    idk po

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
    function (session) {
-      session.beginDialog("hello");
+      session.beginDialog("hello");}
       //session.send(response);}
    ]).set('storage', inMemoryStorage);
 
    bot.dialog("hello",[
-      function (session) {
+      function ( session) {
+         session.dialogData.loca={};
          session.send("");
          session.sendTyping();
          setTimeout(function () {
@@ -33,14 +34,36 @@ var bot = new builder.UniversalBot(connector, [
             session.sendTyping();
          }, 1000);
          setTimeout(function () {
-            session.dialogData.location = "Katipunan";
-            session.send(`You are near ${session.dialogData.location}. Do you want to eat here?`);
+            session.dialogData.loca.location = "Katipunan";
+            session.send(`I think that you're somewhere in ${session.dialogData.location}.`);
             session.sendTyping();
-         }, 3500);
+         }, 3700);
          setTimeout(function () {
-            builder.Prompts.choice(session,"Yes or no?",["Yes", "No"],{listStyle: 3});
-         }, 4500);},
-         function(session, results){
-            session.endDialogWithResult(results.response.entity);
-            }
-         ]);
+            builder.Prompts.choice(session,"Do you want to eat here?",["Yes", "No"],{listStyle: 3});
+         }, 4500);
+      },
+      function( session, results){
+         session.dialogData.hereBa = results.response.entity;
+
+      },
+      function( session){
+         if( session.dialogData.hereBa == "No"){
+            builder.Prompts.text( session, "Oh okay, let's go on an adventure then! Where do you want to eat?");
+         }
+      },
+      function( session, results){
+         if( session.dialogData.hereBa == "No"){
+            session.dialogData.location = results.response.entity;
+         }
+      },
+      function( session){
+         builder.Prompts.text( session, "What are you craving for?");
+      },
+      function( session, results){
+         session.dialogData.location = results.response.entity;
+         setTimeout(function () {
+            session.send();
+            session.sendTyping();
+         }, 1000);
+      },
+   ]);
