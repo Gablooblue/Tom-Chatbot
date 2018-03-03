cd39491739d435d8dee5f51f99ef435acdbbc5f2
cheskape <peraltacheska@gmail.com>
2018-03-04 02:36:48 +0800

    hindi pala siya dialog :(( saddd

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+   appId: process.env.MicrosoftAppId,
+   appPassword: process.env.MicrosoftAppPassword
 });
 
 // Listen for messages from users
@@ -20,30 +20,27 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
    function (session) {
-      session.beginDialog("hello");	}
+      session.beginDialog("hello");
+      //session.send(response);}
    ]).set('storage', inMemoryStorage);
 
-bot.dialog("hello",[
-    function (session) {
-      session.send("");
-      session.sendTyping();
-      setTimeout(function () {
-         session.send("Hello!");
+   bot.dialog("hello",[
+      function (session) {
+         session.send("");
          session.sendTyping();
-      }, 1000);
-      setTimeout(function () {
-   	  session.send("You are near <location>. Do you want to eat here?");
-        session.sendTyping();
-     }, 3500);
-     setTimeout(function () {
-        builder.Prompts.choice(session,"Yes or no?",["Yes", "No"],{listStyle: 3});
-     }, 4500);},
-	function(session, results){
-		session.dialogData.choicer = results.response.entity;
-      if( results.response.entity == "Yes"){
-         session.send("hi");
-      }else{
-         
-      }
-	}
-]);
+         setTimeout(function () {
+            session.send("Hello!");
+            session.sendTyping();
+         }, 1000);
+         setTimeout(function () {
+            session.dialogData.location = "Katipunan";
+            session.send(`You are near ${session.dialogData.location}. Do you want to eat here?`);
+            session.sendTyping();
+         }, 3500);
+         setTimeout(function () {
+            builder.Prompts.choice(session,"Yes or no?",["Yes", "No"],{listStyle: 3});
+         }, 4500);},
+         function(session, results){
+            session.endDialogWithResult(results.response.entity);
+            }
+         ]);
